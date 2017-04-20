bda92ec70f528d089a5ddf46db4e310e5adf4706
rovera19920627 <qiyingzhangbigdata@163.com>
2017-04-20 21:29:15 +0800

    init myWeb

---
+++ js/customer.js
@@ -0,0 +1,151 @@
+var stopTyping=false;
+var picComment1="<div id='content'><div id='code'><span class='comments'>/**</span><br /><span class='space'/><span class='comments'>*Qiying Zhang</span><br /><span class='space'/><span class='comments'>*/</span><br /><span class='keyword'>Gender</span> female;<br /><span class='keyword'>Email</span> zqy.yy@qq.com;<br /><span class='keyword'>Phone</span> 18810543313;<br /><span class='keyword'>String</span> [] education;<br /><span class='keyword'>String</span> [] awards;<br /><span class='keyword'>int</span> i = 0;<br />education[0] = <span class='string'>\"2011-2015 BUPT Computer Science and Technology Bachelor\"</span>;<br />education[1] = <span class='string'>\"2015-now BUPT Computer Science and Technology Master\"</span>;<br />awards[i++] =  <span class='string'>\"2015-2016 Graduate National First Scholarship\"</span>;<br />awards[i++] =  <span class='string'>\"2014-2015 BUPT Second Scholarship\"</span>;<br />awards[i++] =  <span class='string'>\"2014-2015 Arts and Sports Activists Prize of BUPT\"</span>;<br />awards[i++] =  <span class='string'>\"2014 Mentonous Winner in the Mathematical Contest in Modeling\"</span>;<br />awards[i++] =  <span class='string'>\"2013-2014 BUPT Second Scholarship\"</span>;<br />awards[i++] =  <span class='string'>\"2013-2014 Arts and Sports Activists Prize of BUPT\"</span>;<br />awards[i++] =  <span class='string'>\"2013 China Undergraduate Mathematical Contest in Modeling Second Prize in Beijing division\"</span>;<br />awards[i++] =  <span class='string'>\"2012-2013 BUPT Second Scholarship\"</span>;<br />awards[i++] =  <span class='string'>\"2011-2012 Arts and Sports Activists Prize of BUPT\"</span>;<br /></div></div>";
+var picComment2="<div id='content'><div id='code'><span class='comments'>/**</span><br /><span class='space'/><span class='comments'>*2011-09</span><br /><span class='space'/><span class='comments'>*/</span><br /><span class='keyword'>Loction</span> nameLocation = BUPT;<br /><span class='keyword'>Classification</span> nameClassification = activity;<br /><span class='comments'>// I was selected as one of the intern school magazine editors during collage military training on the very first day. </span><br />The job included recording collage military training for freshmen as well as writing foreword for the magazine;<br /><span class='placeholder'/><span class='comments'>// After military training, I formally worked as an editor in chief of mobile news, </span><br /><span class='placeholder'/><span class='comments'>// which is a new department of the school magazine.</span><br />In addition to editing the fundamental contents of daily news, <br />I was also required to cooperate with student council to get the first-hand information of student activities;<br /></div></div>";
+var picComment3="";
+var picComment4="<div id='content'><div id='code'><span class='comments'>/**</span><br /><span class='space'/><span class='comments'>*2011-10</span><br /><span class='space'/><span class='comments'>*/</span><br /><span class='keyword'>Loction</span> nameLocation = BUPT;<br /><span class='keyword'>Classification</span> nameClassification = show;<br /><span class='keyword'>String </span>award = <span class='string'>\"third place in Campus Singer Competition\";</span><br /><span class='placeholder'/><span class='comments'>//[animal city-Shakira && poker face--Lady Gaga && haunted -- Taylor Swift]</span><br /></div></div>"
+var picComment5="<div id='content'><div id='code'><span class='comments'>/**</span><br /><span class='space'/><span class='comments'>*2011-9---2013-9,</span><br /><span class='space'/><span class='comments'>*/</span><br /><span class='keyword'>Loction</span> nameLocation = BUPT;<br /><span class='keyword'>Classification</span> nameClassification = student council;<br /><span class='keyword'>Department</span> nameDepartment1 = <span class='string'>\"Organization Department of Information Technology Academy\";</span><br /><span class='keyword'>Position</span> namePosition1 = <span class='string'>\"Minister\";</span><br /><span class='keyword'>Department</span> nameDepartment2 = <span class='string'>\"School Public Relations Department\";</span><br /><span class='keyword'>Position</span> namePosition2 = <span class='string'>\"Deputy Minister\";</span><br />Besides being in charge of the organization of student activities such as Debate Competition, Appraising of League Community etc., <br />I am also responsible for looking for sponsors for the activities. <br /><span class='comments'> //Not only the work itself but also cooperating and leading makes me become more mature in mind. </span><br /><span class='comments'> //Effective leadership gives me a strong team and sincere friendship. <br /></div></div>";
+var picComment6="";
+var picComment7="<div id='content'><div id='code'><span class='comments'>/**</span><br /><span class='space'/><span class='comments'>*2011-3</span><br /><span class='space'/><span class='comments'>*/</span><br /><span class='keyword'>Loction</span> nameLocation = PKU;<br /><span class='keyword'>Classification</span> nameClassification = activities;<br /><span class='keyword'>Department</span> nameDepartment = <span class='string'>\"Aiesec PKU\";</span><br /><span class='comments'> //Aiesec:Association Internationale des Etudiants en Sciences Economiques et Commerciales</span><br /><span class='comments'> //PKU:Peking University</span><br />It helped me get to know a lot of students from different schools and majors. <br />The girl from PKU medical major inspired me with her focusing on work, <br />while the boy from RUC economic major made me feel the charm of active mind. <br /><span class='comments'> //RUC:RENMIN University of China</span><br />Five month later, <br />I became a member of the organization team for RIC in Beijing <br /> <span class='comments'> //RIC:Regional Introduction Conference</span><br />and organized a conference lasted for three days receiving more than 2000 students from different colleges.<br /> In addition to the experience of struggling to overcome difficulties,<br /> it also made me feel the enthusiasm from same-age young people trying to make the world a little bit different by becoming a better person. <br /></div></div>";
+var picComment8="";
+var picComment9="<div id='content'><div id='code'><span class='comments'>/**</span><br /><span class='space'/><span class='comments'>*2013-7--2013-9</span><br /><span class='space'/><span class='comments'>*/</span><br /><span class='keyword'>Loction</span> nameLocation = XiangTan, HuNan Province;<br /><span class='keyword'>Classification</span> nameClassification = activities;<br />I participated in a teaching activity in Hunan province together with 14 Chinese and seven foreign friends from Netherlands, Cambodia, Russia, Vietnam and Egypt.<br />It took about four hours driving from Xiangtan City to the village. <br />We spent more than a month there teaching children English. <br />Nowadays, there are a lot of voices vote against teaching activities. <br />It is true that there are a lot of students participating such activities only for additions on resume. <br />It is also true that the activity itself may not change nothing at all since the children there can hardly afford for higher education. But as far as I am concerned, life is all about experiences. <br />Try of showing children a different world and wholeheartedly immerse in better teaching gave us all, including children, foreign volunteers and us, unforgettable experience.<br /></div></div>";
+var picComment10="";
+var picComment11="<div id='content'><div id='code'><span class='comments'>/**</span><br /><span class='space'/><span class='comments'>*2013-10</span><br /><span class='space'/><span class='comments'>*/</span><br /><span class='keyword'>Loction</span> nameLocation = BUPT;<br /><span class='keyword'>Classification</span> nameClassification = show;<br /><span class='keyword'>String </span>showTime = <span class='string'>\"Farewell theme party\";</span><br /><span class='placeholder'/><span class='comments'>//[Bad influence -- P!nk]</span><br /></div></div>";
+var picComment12="<div id='content'><div id='code'><span class='comments'>/**</span><br /><span class='space'/><span class='comments'>*2014-2</span><br /><span class='space'/><span class='comments'>*/</span><br /><span class='keyword'>Loction</span> nameLocation = BUPT;<br /><span class='keyword'>Classification</span> nameClassification = coding;<br /><span class='keyword'>Competition</span> nameCompetition = MCM/ICM;<br /><span class='keyword'>String </span>award = <span class='string'>\"Meritorious Winner\"</span><br />In order to model coach ranking,<br />we chose discrete hopfield neural network as the baseline method <br />and applied analytic hierarchy process to further analysis the hidden variables. <br />Since the hidden variables are hard to define, FMADM was used to deal with fuzzy property. <span class='comments'> //it worked just well.</span> <br />The entire process of competition inspired my interest in machine leaning.<br /></div></div>";
+var picComment13="<div id='content'><div id='code'><span class='comments'>/**</span><br /><span class='space'/><span class='comments'>*2015-6</span><br /><span class='space'/><span class='comments'>*/</span><br /><span class='keyword'>Loction</span> nameLocation = BUPT;<br /><span class='keyword'>Classification</span> nameClassification = show;<br /><span class='keyword'>String </span>showTime = <span class='string'>\"Graduation party\";</span><br />I participated in three shows out of 15 in total including an openning dance.<br /></div></div>";
+var picComment14="";
+var picComment15="";
+var picComment16="<div id='content'><div id='code'><span class='comments'>/**</span><br /><span class='space'/><span class='comments'>*2016-3</span><br /><span class='space'/><span class='comments'>*/</span><br /><span class='keyword'>Loction</span> nameLocation = BUPT;<br /><span class='keyword'>Classification</span> nameClassification = coding;<br /><span class='keyword'>String </span>award = <span class='string'>\"Third prize in Programming Contest of BUPT\";</span><br />I have never received ACM training before. <br /> after practicing with my teammates for a month, we won a little prize in the competition.<br /> <span class='comments'> //But the most important thing is,<br /> I felt the brainstorming as a coder.<br /></span></div></div>";
+var picComment17="<div id='content'><div id='code'><span class='comments'>/**</span><br /><span class='space'/><span class='comments'>*2016-6</span><br /><span class='space'/><span class='comments'>*/</span><br /><span class='keyword'>Loction</span> nameLocation = BUPT;<br /><span class='keyword'>Classification</span> nameClassification = coding;<br /><span class='keyword'>String </span>award = <span class='string'>\"Outstanding Undergraduate Graduation Dissertation\";</span><br />I graduated from Beijing University of Posts and Telecommunications in 2015. <br />My dissertation named \"Design and Implementation of Spatio-Textual Similarity Joins Authentication Algorithms\" won the outstanding undergraduate graduation dissertation that year.<br /></div></div>";
+var picComment18="<div id='content'><div id='code'><span class='comments'>/**</span><br /><span class='space'/><span class='comments'>*2016-7--2016-11</span><br /><span class='space'/><span class='comments'>*/</span><br /><span class='keyword'>Loction</span> nameLocation = Baidu;<br /><span class='keyword'>Classification</span> nameClassification = coding;<br /><span class='keyword'>String </span>Department = <span class='string'>\"Baidu Image Search Architecture Department\";</span><br />I worked as an intern in Baidu for 4 months in in Baidu Image Search Architecture Department.<br /> After being familiar with the offline index portion and MapReduce jobs as well, I participated in the shielding optimization of image architecture.<br />  In the first project, I was in charge of building a backup trust URL index of online shield. <br />  My introducing of inverted list inspired by text search simplified the program procedure efficiently and was highly commended by the engineer manager. <br /> After the first project, I was required to participate in the development of the basic visualized web tool, which pouring the general image database for QA. <br /> <span class='comments'> //Since I didn’t have such coding experience, I took a quick study and immersed in work as fast as I can.  </span><br />In the final project as an intern in Baidu, I was able to in charge of the development of a web tool checking the result of building offline database, which works efficiently as an internal project now.<br /></div></div>";
+var picComment19="<div id='content'><div id='code'><span class='comments'>/**</span><br /><span class='space'/><span class='comments'>*Now</span><br /><span class='space'/><span class='comments'>*/</span><br /><span class='keyword'>Loction</span> nameLocation = BUPT;<br /><span class='keyword'>Classification</span> nameClassification = coding;<br /><span class='keyword'>Competition</span> nameCompetition = Alibaba middleware competition;<br />Now I am a master from the State Key Laboratory of Networking and Switching Technology in Beijing University of Posts and Telecommunications.<br /><span class='comments'> //After spending half a year reading dissertations and implementing algorithms.</span><br /> The challenge of Alibaba middleware competition gives me more comprehensive understanding about programming. <br /> The topic of preliminary round gives me the opportunity of getting to know storm, rocketmq, as well as tair, while the semi-finals makes me spend a long time balancing between capabilities of different parts. <br /> In spite of lacking enough support from teammate, I tried my best in program optimization and made it in the eight place in semi-finals.<br /><span class='keyword'>Hoping</span> <span class='string'>study tour;</span><br />In the study tour I'm applying for, I want to get to know a broader technology circle which committed to changing the world. <br />My dream is to make world a little bit better because of my hard working as a coder. <br />Like-minded friends and opportunity of in-depth study attract me too much that I want to try my best to get the opportunity. <br />I may not be that smart like thousands of geeks working in this field, but I will do my best. After all, life is impressing after struggling trying.<br /></div></div>";
+var picComment20="";
+                
+            
+
+window.onload=changeMainContainer;
+window.onresize=changeMainContainer;
+$(function(){
+	$('img.hoverpic').each(function(){}).hover(function(){$(this).animate({opacity:0.4});}, function(){$(this).animate({opacity:1});});
+	$('#pic1').click(function(){
+        $('.bgCover').fadeIn(400);
+        $('.outWindow').fadeIn(800);
+        $('.outWindow').html(picComment1);
+        $("#code").typewriter();
+    });
+	$('#pic2').click(function(){
+        $('.bgCover').fadeIn(400);
+        $('.outWindow').fadeIn(800);
+        $('.outWindow').html(picComment2);
+        $("#code").typewriter();
+    });
+
+    $('#pic4').click(function(){
+        $('.bgCover').fadeIn(400);
+        $('.outWindow').fadeIn(800);
+        $('.outWindow').html(picComment4);
+        $("#code").typewriter();
+    });
+    $('#pic5').click(function(){
+        $('.bgCover').fadeIn(400);
+        $('.outWindow').fadeIn(800);
+        $('.outWindow').html(picComment5);
+        $("#code").typewriter();
+    });
+    $('#pic7').click(function(){
+        $('.bgCover').fadeIn(400);
+        $('.outWindow').fadeIn(800);
+        $('.outWindow').html(picComment7);
+        $("#code").typewriter();
+    });
+    $('#pic9').click(function(){
+        $('.bgCover').fadeIn(400);
+        $('.outWindow').fadeIn(800);
+        $('.outWindow').html(picComment9);
+        $("#code").typewriter();
+    });
+
+    $('#pic11').click(function(){
+        $('.bgCover').fadeIn(400);
+        $('.outWindow').fadeIn(800);
+        $('.outWindow').html(picComment11);
+        $("#code").typewriter();
+    });
+    $('#pic12').click(function(){
+        $('.bgCover').fadeIn(400);
+        $('.outWindow').fadeIn(800);
+        $('.outWindow').html(picComment12);
+        $("#code").typewriter();
+    });
+    $('#pic13').click(function(){
+        $('.bgCover').fadeIn(400);
+        $('.outWindow').fadeIn(800);
+        $('.outWindow').html(picComment13);
+        $("#code").typewriter();
+    });
+    $('#pic16').click(function(){
+        $('.bgCover').fadeIn(400);
+        $('.outWindow').fadeIn(800);
+        $('.outWindow').html(picComment16);
+        $("#code").typewriter();
+    });
+    $('#pic17').click(function(){
+        $('.bgCover').fadeIn(400);
+        $('.outWindow').fadeIn(800);
+        $('.outWindow').html(picComment17);
+        $("#code").typewriter();
+    });
+    $('#pic18').click(function(){
+        $('.bgCover').fadeIn(400);
+        $('.outWindow').fadeIn(800);
+        $('.outWindow').html(picComment18);
+        $("#code").typewriter();
+    });
+    $('#pic19').click(function(){
+        $('.bgCover').fadeIn(400);
+        $('.outWindow').fadeIn(800);
+        $('.outWindow').html(picComment19);
+        $("#code").typewriter();
+    });
+    
+    $('.bgCover').click(function() {
+        $('.bgCover').fadeOut(800);
+        $('.outWindow').fadeOut(400);
+        stopTyping;
+        $('.outWindow').empty();
+    });
+});
+
+function changeMainContainer() {
+	var divOne=document.getElementById('total-container');
+	divOne.style.height=divOne.offsetWidth*0.67+'px';
+}
+
+(function($) {
+	stopTyping=false;
+	$.fn.typewriter = function() {
+		this.each(function() {
+			if(stopTyping)
+				return;
+			var $ele = $(this), str = $ele.html(), progress = 0;
+			$ele.html('');
+			var timer = setInterval(function() {
+				var current = str.substr(progress, 1);
+				if (current == '<') {
+					progress = str.indexOf('>', progress) + 1;
+				} else {
+					progress++;
+				}
+				$ele.html(str.substring(0, progress) + (progress & 1 ? '_' : ''));
+				if (progress >= str.length) {
+					clearInterval(timer);
+				}
+			}, 75);
+		});
+		return this;
+	};
+})(jQuery);
+
+function stopTyping() {
+	stopTyping=true;
+}
+
